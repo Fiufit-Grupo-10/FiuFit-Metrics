84ccefcd04c6c65e1bc7262bfef6be8b08ae6937
Franco Gazzola <fgazzola@fi.uba.ar>
2023-06-04 18:55:07 -0300

    env variable error fixed

--- app/config/database.py
+++ app/config/database.py
@@ -2,7 +2,7 @@ import os
 
 # import motor.motor_asyncio
 
-MONGO_URL = os.getenv("MONGO_METRICS_URL", "")
+MONGO_METRICS_URL = os.getenv("MONGO_METRICS_URL", "")
 METRICS_COLLECTION_NAME = "users"
 DB_NAME = "Metrics"
 
